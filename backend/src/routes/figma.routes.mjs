9af2a7e8281e1a37c437d962c8243588d5d9038d
rishabhy123 <www.rishabhyadav2004@gmail.com>
2026-01-25 18:00:06 +0530

    Created the folder structure

---
+++ backend/src/routes/figma.routes.mjs
@@ -0,0 +1,16 @@
+import express from "express";
+import { getFigmaFile } from "../services/figma.service.mjs";
+
+const router = express.Router();
+
+router.get("/file/:fileId", async (req, res) => {
+  try {
+    const data = await getFigmaFile(req.params.fileId);
+    res.json(data.document);
+  } catch (err) {
+    res.status(500).json({ error: "Figma fetch failed" });
+    console.log(err.message)
+  }
+});
+
+export default router;
---
+++ backend/src/services/figma.service.mjs
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const FIGMA_BASE_URL = "https://api.figma.com/v1";
+
+export const getFigmaFile = async (fileId) => {
+  const res = await axios.get(
+    `${FIGMA_BASE_URL}/files/${fileId}`,
+    {
+      headers: {
+        "X-Figma-Token": process.env.FIGMA_TOKEN
+      }
+    }
+  );
+  return res.data;
+};
