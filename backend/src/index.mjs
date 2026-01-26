8c2f55e525ff6839f7ce7ce646c8be44aa4d746b
rishabhy123 <www.rishabhyadav2004@gmail.com>
2026-01-26 11:07:17 +0530

    Function to get the data from Figma Api

--- backend/src/index.mjs
+++ backend/src/index.mjs
@@ -10,8 +10,21 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/figma", figmaRoutes);
-console.log("Token:", process.env.FIGMA_TOKEN ? "Present" : "Missing!");
+// app.get('/test',async(req, res)=>{
+//   try{
+//   const r = await fetch("https://api.figma.com/v1/me", {
+//     headers:{
+//       "X-Figma-Token": process.env.FIGMA_TOKEN
+//     }
+//   })
+//   const data = await r.json();
+//   res.json(data);
+// }
+// catch(e){
+//   res.status(500).json({ error: e.message });
+// }
 
+// })
 app.listen(process.env.PORT, () => {
   console.log("Server running on port", process.env.PORT);
 });
